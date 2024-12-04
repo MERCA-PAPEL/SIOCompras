3c49d53c7488cfba589fcc762d76a012cfa89e85
Ricardo Alonso Medina <sistemas3@mercapapel.com>
2024-12-04 16:27:46 -0600

    init

---
+++ src/test/java/com/mercapapel/Compras/ComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercapapel.Compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
